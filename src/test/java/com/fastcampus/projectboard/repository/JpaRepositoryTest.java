36df317066ed6c3c1dbecf57566dbc33ddb98771
hyerin <tytntu@gmail.com>
2022-07-19 00:37:14 +0900

    #13 - article, acticleComment entity 설정
    entity 설정 후 equals, hashcode 재정의 및 cascade 설정

---
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest    // slice test / junit5 부터는 @Autowired 대신에 생성자 주입 패턴을 사용할 수 있다.
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // given
+
+        // when
+
+        // then
+    }
+}
