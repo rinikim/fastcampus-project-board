d3f2110ecf96bcea18aa8fc73cf93b42726cd41f
hyerin <tytntu@gmail.com>
2022-07-20 22:26:50 +0900

    #1 - Spring Data REST 테스트 제외처리
    이것은 이 프로젝트의 비즈니스 로직으로 구현한 내용이 아니라 data rest 기능이고
    통합테스트라 무거우며 db에도 영향을 준다
    공부 목적으로도 의미는 있기 때문에 삭제하지는 않고
    제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -3,6 +3,7 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
