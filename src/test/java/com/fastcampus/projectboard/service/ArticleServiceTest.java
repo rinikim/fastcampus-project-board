10ca7b9defa71b5d579d260d297d79f72a68d089
hyerin <tytntu@gmail.com>
2022-08-21 01:21:18 +0900

    강의 외 변경사항 반영

--- src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -2,7 +2,7 @@ package com.fastcampus.projectboard.service;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.UserAccount;
-import com.fastcampus.projectboard.domain.type.SearchType;
+import com.fastcampus.projectboard.domain.constant.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
