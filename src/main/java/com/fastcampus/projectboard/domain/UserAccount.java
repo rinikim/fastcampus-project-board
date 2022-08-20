4b3d9220769cde3295de2216a1a606ded36c2f37
hyerin <tytntu@gmail.com>
2022-08-20 18:32:01 +0900

    #32 - userId unique key 설정

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 @Getter
 @ToString
 @Table(indexes = {
+        @Index(columnList = "userId", unique = true),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
